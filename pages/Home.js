24fd23bf6155c8a536fce5d64d5e271db4a942c1
Daniel Zordan <danielzordanfilho@gmail.com>
2023-08-07 18:05:16 +0000

    adiciona arquivo `.prettierignore`

--- pages/Home.js
+++ pages/Home.js
@@ -1,28 +1,9 @@
-import { useState, useEffect } from "react";
 import styles from "./Home.module.css";
 
 function Home() {
-  const [showText, setShowText] = useState(true);
-
-  function blink() {
-    setShowText(false);
-  }
-
-  useEffect(() => {
-    setInterval(() => {
-      blink();
-    }, 50000);
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowText(true);
-    }, 1000);
-  }, [blink]);
-
   return (
     <div className={styles.homediv}>
-      {showText && <h1>ESTOU DE OLHO EM VOCÊ</h1>}
+      <h1>HELLO WORLD</h1>
     </div>
   );
 }
